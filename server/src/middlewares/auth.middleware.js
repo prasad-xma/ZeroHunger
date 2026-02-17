a2130c87b687cdd6dba3e8f71011391410639579
prasad-xma <prasadmanamperi130@gmail.com>
2026-02-18 01:19:45 +0530

    implement auth middleware to controll route access

---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+const User = require('../modules/users/user.model');
+
+const protect = async (req,res, next) => {
+    let token;
+    
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+        token = req.headers.authorization.split(" ")[1];
+    }
+
+    if (!token) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = await User.findById(decoded.id).select("-password");
+        next();
+
+    } catch (error) {
+        console.error(`Auth middleware error: ${error.message}`);
+        return res.status(401).json({ message: "Unauthorized" });
+    
+    }
+}
+
+module.exports = {
+    protect,
+}
